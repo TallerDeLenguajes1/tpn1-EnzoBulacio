4e1eefb106c58fac50261c7ae2abaf5a33d1ced6
Enzo <enzobulacio18@gmail.com>
2020-03-31 00:04:50 -0300

    agrego los puntos a b c d tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int num,*dir_num;
+    num=20;
+    dir_num=&num;
+    cout<<"Numero: "<<num<<endl;
+    cout<<"x"<<*dir_num<<endl;
+	cout<<"direccion de memoria del la variable: "<<&num<<endl; //deireccion de memoria
+    cout<<"direccion de memoria del puntero almacenado la variable: "<<dir_num<<endl;
+    getch();
+    return 0;
+}
